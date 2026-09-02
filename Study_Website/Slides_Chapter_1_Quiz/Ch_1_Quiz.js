719ac078e0e3cf67681114ee4f5e59257fd58cf3
ValWasHere <vallaguda@gmail.com>
2026-09-02 12:07:10 -0500

    Add files via upload

---
+++ Study_Website/Slides_Chapter_1_Quiz/Ch_1_Quiz.js
@@ -0,0 +1,277 @@
+function checkAnswerOne(answer){
+    const resultDiv = document.getElementById('outputDisplayOne');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Correct!";
+    }
+}
+function checkAnswerTwo(answer){
+    const resultDiv = document.getElementById('outputDisplayTwo');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Correct!<br><br>An example of structured data is data in excel spreadsheets.";
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerThree(answer){
+    const resultDiv = document.getElementById('outputDisplayThree');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Correct!<br><br>Mozzarella Firefox is not real!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Incorrect!"
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerFour(answer){
+    const resultDiv = document.getElementById('outputDisplayFour');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Correct!";
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerFive(answer){
+    const resultDiv = document.getElementById('outputDisplayFive');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Correct!";
+    }
+}
+function checkAnswerSix(answer){
+    const resultDiv = document.getElementById('outputDisplaySix');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Correct!";
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerSeven(answer){
+    const resultDiv = document.getElementById('outputDisplaySeven');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Correct!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerEight(answer){
+    const resultDiv = document.getElementById('outputDisplayEight');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Correct!";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerNine(answer){
+    const resultDiv = document.getElementById('outputDisplayNine');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Correct!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerTen(answer){
+    const resultDiv = document.getElementById('outputDisplayTen');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerB){
+        resultDiv.innerHTML = "Incorrect!";
+    }else if(answer === answerC){
+        resultDiv.innerHTML = "Correct!";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerEleven(answer){
+    const resultDiv = document.getElementById('outputDisplayEleven');
+    const answerA = 'A';
+    const answerB = 'B';
+    const answerC = 'C';
+    const answerD = 'D';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Correct!<br><br>Data is the result of processing raw facts to reveal their meaning.";
+    }
+}
+function checkAnswerTwelve(answer){
+    const resultDiv = document.getElementById('outputDisplayTwelve');
+    const answerA = 'A';
+    const answerB = 'B';
+    
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Correct!<br><br>Database systems require sophisticated hardware, software, and highly skilled personnel. This increases costs.";
+    }
+    
+}
+function checkAnswerThirteen(answer){
+    const resultDiv = document.getElementById('outputDisplayThirteen');
+    const answerA = 'A';
+    const answerB = 'B';   
+    if(answer === answerA){
+        resultDiv.innerHTML = "Correct!";
+    }else{
+        resultDiv.innerHTML = "Inorrect!";
+    }
+}
+function checkAnswerFourteen(answer){
+    const resultDiv = document.getElementById('outputDisplayFourteen');
+    const answerA = 'A';
+    const answerB = 'B';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Correct!";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerFifteen(answer){
+    const resultDiv = document.getElementById('outputDisplayFifteen');
+    const answerA = 'A';
+    const answerB = 'B';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Correct! Unstructured data is in its raw state while structured data is formatted to enable storage and generation of meaningful information.";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerSixteen(answer){
+    const resultDiv = document.getElementById('outputDisplaySixteen');
+    const answerA = 'A';
+    const answerB = 'B';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Correct! It is true that <b><u>structural independence</u></b> exists when it is possible to make changes in the file structure without affecting the application program's ability to access the data, not <b><u>structural dependence</b></u>.";
+    }
+}
+function checkAnswerSeventeen(answer){
+    const resultDiv = document.getElementById('outputDisplaySeventeen');
+    const answerA = 'A';
+    const answerB = 'B';    
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Correct!  It is vital for the database designer to determine which data to enter into the database and how to organize that data.";
+    }
+}
+function checkAnswerEighteen(answer){
+    const resultDiv = document.getElementById('outputDisplayEighteen');
+    const answerA = 'A';
+    const answerB = 'B';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Correct! The data warehouse stores historical data obtained from many sources.";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+function checkAnswerNineteen(answer){
+    const resultDiv = document.getElementById('outputDisplayNineteen');
+    const answerA = 'A';
+    const answerB = 'B';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Incorrect!";
+    }else{
+        resultDiv.innerHTML = "Correct! A well designed database is essential for a DBMS to perform well. ";
+    }
+}
+function checkAnswerTwenty(answer){
+    const resultDiv = document.getElementById('outputDisplayTwenty');
+    const answerA = 'A';
+    const answerB = 'B';
+    if(answer === answerA){
+        resultDiv.innerHTML = "Correct! Data redundancy exists when data is duplicated and stored unnecessarily at different places.";
+    }else{
+        resultDiv.innerHTML = "Incorrect!";
+    }
+}
+/* 
+This was originally used for each clear button, which was a waste of space when coding (i.e. redundant).
+const clearResultOne = () => document.getElementById('outputDisplayOne').innerHTML = "One of these four are correct, choose wisely.";//Redundant
+const clearResultTwo = () => document.getElementById('outputDisplayTwo').innerHTML = "One of these four are correct, choose wisely.";//Redundant
+const clearResultThree = () => document.getElementById('outputDisplayThree').innerHTML = "One of these four are correct, choose wisely.";//Redundant
+*/
+document.addEventListener('DOMContentLoaded', () => {
+    const clearText = "Answer Reveal.";
+    function clearAllOutputs(){
+        document.querySelectorAll('.outputStyle').forEach(el => el.innerHTML = clearText);
+    }
+    document.getElementById('clearAll').addEventListener('click', clearAllOutputs);
+});//Clears all "Answer Reveal" output div's with one button.
